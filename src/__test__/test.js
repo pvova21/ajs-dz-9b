d7b78f7e5a74bb8697a31ed0663467b6216d2bde
pvova21 <podrezov08@rambler.ru>
2022-11-07 12:07:19 +0300

    1 commit

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,3 +1,17 @@
-test('should add two numbers', () => {
-  expect(2).toBe(2);
+import ArrayBufferConverter, { getBuffer } from '../index';
+
+test('проверка выполнения кода', () => {
+  const buffer = getBuffer();
+  const test = new ArrayBufferConverter();
+  test.load(buffer);
+  const received = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  expect(received).toEqual(test.toString());
+});
+
+test('проверка типа данных', () => {
+  const buffer = getBuffer();
+  const test = new ArrayBufferConverter();
+  test.load(buffer);
+  const received = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  expect(typeof (received)).toEqual(typeof (test.toString()));
 });
